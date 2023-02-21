cf621b1f7e3ee7dcec628ecef790fb2829eba29a
WangEz <oncemiss1@163.com>
2023-02-21 22:03:45 +0800

    WangEz, feat: init ddd project

---
+++ chatbot-api-interfaces/src/main/java/cn/bugstack/chatbot/api/ApiApplication.java
@@ -0,0 +1,17 @@
+package cn.bugstack.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动入口
+ *
+ * @author ：ez
+ * @since ：2023/2/21 17:33
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/cn/bugstack/chatbot/api/test/ApiTest.java
@@ -0,0 +1,10 @@
+package cn.bugstack.chatbot.api.test;
+
+/**
+ * 测试
+ *
+ * @author ：ez
+ * @since ：2023/2/21 21:57
+ */
+public class ApiTest {
+}
